90a9cd8d93631aae16e46ef280a3d096fdc05643
belkacem9 <mamoubelkacem99@gmail.com>
2023-06-25 19:20:01 +0200

    second commit

--- app/app.py
+++ app/app.py
@@ -4,3 +4,30 @@ from db import database
 
 app = fastapi()
 
+@app.get("/voiture")
+def get_voitures():
+    db = database()
+    voitures = db.warehouse.find()
+    res = []
+
+    for voiture in voitures:
+        data = {
+            "immatriculation": voiture["immatriculation"],
+            "marque": voiture["marque"],
+            "modele": voiture["modele"],
+            "prix": voiture["prix"],
+            "color": voiture["color"],
+        }
+        res.append(data)
+    return res
+
+@app.post("/voiture")
+def create_voiture(voiture: Voiture):
+    db = database()
+    
+
+@app.get("/voiture/{immat}")
+
+@app.put("/voiture/{immat}")
+
+@app.delete("/voiture/{immat}")
